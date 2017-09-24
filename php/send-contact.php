79e57f1466b55fb57a9ae0025d5483115f602730
Raghav <raghav96@github.com>
2017-09-23 17:01:30 -0700

    Changed php form

--- php/send-contact.php
+++ php/send-contact.php
@@ -7,7 +7,7 @@
 	$message = @trim(stripslashes($_POST['message'])); 
 
 	$email_from = $email;
-	$email_to = 'email@yourdomain.com';//replace with your email
+	$email_to = 'raghav96@gmail.com';//replace with your email
 
 	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Website: ' . $website . "\n\n" . 'Message: ' . $message;
 
